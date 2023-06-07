89d337455507b4878a0901fbcea14988b5b06de6
Ashlay <105703068+AshlayMeijer@users.noreply.github.com>
2023-06-07 19:48:03 +0200

    Move things around

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
+/* eslint-disable no-unused-vars */
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const functions = require("./functions");
 
 /*
     Homepage
 */
-router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../public', 'html/index.html'));
+router.get("/", async (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../public/html/index.html'));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -39,13 +39,14 @@ app.use(morgan('dev'));
 
 
 /* Setting routes */
-app.use('/', require('./routes/index'));
 app.use('/css', express.static('public/css'));
 app.use('/js', express.static('public/js'));
 app.use('/html', express.static('public/html'));
 app.use('/images', express.static('public/images'));
 app.use('/json', express.static('json'));
 app.use('/CustomComponents', express.static('public/components'));
+
+app.use('/', require('./routes/index'));
 
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
