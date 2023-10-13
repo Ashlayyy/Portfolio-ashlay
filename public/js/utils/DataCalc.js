dc0a669c79e6cbff7cc1600d91eee7d453f4c001
Ashlay <105703068+AshlayMeijer@users.noreply.github.com>
2023-10-13 19:32:00 +0200

    Trying out dayDiff

--- public/js/utils/DataCalc.js
+++ public/js/utils/DataCalc.js
@@ -6,15 +6,17 @@ class DataCalc {
 
     constructor (dateBegon, dateNow) {
         dateBegon = new Date(dateBegon);
+        const dayDiff = dateNow.getDay() - dateBegon.getDay();
         const monthDiff = dateNow.getMonth() - dateBegon.getMonth();
         const yearDiff = dateNow.getYear() - dateBegon.getYear();
         this.months = monthDiff + yearDiff * 12;
+        this.days = dayDiff;
 
         while(this.months >= 12) {
             this.years = this.years + 1;
             this.months = this.months - 12;
         }
-
+        console.log(this.days)
         if (this.years === 1) {
             this.result = `${this.years} jaar`;
         } else if (this.years > 1) {
