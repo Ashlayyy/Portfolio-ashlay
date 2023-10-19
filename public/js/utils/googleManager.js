f42eb073fae715714da97345532dbcb87556758a
Ashlay <105703068+AshlayMeijer@users.noreply.github.com>
2023-10-19 15:37:21 +0200

    Updated

--- public/js/utils/googleManager.js
+++ public/js/utils/googleManager.js
@@ -1,9 +1,4 @@
 window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-1CMHDSBP62');
-
-window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-1JZMRB4SKT');
