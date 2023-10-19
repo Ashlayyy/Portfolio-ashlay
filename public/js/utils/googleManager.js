51f230764d2d9967522a63e11608f17762e85f04
Ashlay <105703068+AshlayMeijer@users.noreply.github.com>
2023-10-19 15:30:42 +0200

    Change account for google thing internal

--- public/js/utils/googleManager.js
+++ public/js/utils/googleManager.js
@@ -2,3 +2,8 @@ window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 gtag('config', 'G-1CMHDSBP62');
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-1JZMRB4SKT');
