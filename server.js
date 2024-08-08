9561fa7415f75a24568a4120b8837d0b0f5a26ca
Ashlay Steur <ashlay.steur@archie.nl>
2024-08-08 22:15:08 +0200

    as

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 /* Requiring all non-local packages */
-const express = require('express')
-const favicon = require('serve-favicon');
-const bodyParser = require('body-parser');
-const compression = require('compression');
-const helmet = require('helmet');
-const morgan = require('morgan');
-require('dotenv').config();
+const express = require("express");
+const favicon = require("serve-favicon");
+const bodyParser = require("body-parser");
+const compression = require("compression");
+const helmet = require("helmet");
+const morgan = require("morgan");
+require("dotenv").config();
 
 /*Requiring all local packages */
 //eslint-disable-next-line no-unused-vars
-const logger = require('./utilities/logger');
+const logger = require("./utilities/logger");
 
 /* Starting app */
 const app = express();
@@ -21,45 +21,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
-app.use(helmet.contentSecurityPolicy({
-  useDefaults: true,
-  directives: {
-    "script-src": [
-      "self", 
-      "https://kit.fontawesome.com/", 
-      "https://www.googletagmanager.com/", 
-      "https://ashlaydev.com/",
-      "https://www.ashlaydev.com/",
-      "https://js-eu1.hs-scripts.com/",
-      "https://js-eu1.hs-banner.com/",
-      "https://js-eu1.hs-analytics.net/",
-      "https://js-eu1.hscollectedforms.net/",
-      "'unsafe-inline'",
-      "*.cloudflare.com"
-    ],
-    "font-src": [
-      "self", 
-      "https://ka-f.fontawesome.com/",
-      "https://ashlaydev.com/",
-      "https://www.ashlaydev.com/",
-      "*.cloudflare.com/"
-    ],
-    "style-src": [
-      "self",
-      "'unsafe-inline'",
-      "https://ashlaydev.com/",
-      "https://www.ashlaydev.com/",
-      "*.cloudflare.com"
-    ],
-    "connect-src": [
-      "self", 
-      "https://ka-f.fontawesome.com/",
-      "https://ashlaydev.com/",
-      "https://www.ashlaydev.com/",
-      "https://*.google-analytics.com/"
-    ]
-  },
-}));
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "script-src": [
+        "self",
+        "https://kit.fontawesome.com/",
+        "https://www.googletagmanager.com/",
+        "https://ashlaydev.com/",
+        "https://www.ashlaydev.com/",
+        "https://js-eu1.hs-scripts.com/",
+        "https://js-eu1.hs-banner.com/",
+        "https://js-eu1.hs-analytics.net/",
+        "https://js-eu1.hscollectedforms.net/",
+        "https://forms-eu1.hscollectedforms.net/",
+        "https://forms-eu1.hsforms.com/",
+        "'unsafe-inline'",
+        "*.cloudflare.com",
+      ],
+      "font-src": [
+        "self",
+        "https://ka-f.fontawesome.com/",
+        "https://ashlaydev.com/",
+        "https://www.ashlaydev.com/",
+        "*.cloudflare.com/",
+      ],
+      "style-src": [
+        "self",
+        "'unsafe-inline'",
+        "https://ashlaydev.com/",
+        "https://www.ashlaydev.com/",
+        "*.cloudflare.com",
+      ],
+      "connect-src": [
+        "self",
+        "https://ka-f.fontawesome.com/",
+        "https://ashlaydev.com/",
+        "https://www.ashlaydev.com/",
+        "https://*.google-analytics.com/",
+      ],
+    },
+  })
+);
 
 app.use(helmet.crossOriginEmbedderPolicy());
 app.use(helmet.crossOriginOpenerPolicy());
@@ -81,25 +85,24 @@ app.use(
   helmet.referrerPolicy({
     policy: "no-referrer",
   })
-)
+);
 app.use(helmet.xssFilter());
-app.use(morgan('dev'));
-
+app.use(morgan("dev"));
 
 /* Setting routes */
-app.use('/css', express.static('./public/css'));
-app.use('/js', express.static('./public/js'));
-app.use('/html', express.static('./public/html'));
-app.use('/images', express.static('./public/images'));
-app.use('/json', express.static('./json'));
-app.use('/CustomComponents', express.static('./public/components'));
-app.use('/download', express.static('./public/download'));
+app.use("/css", express.static("./public/css"));
+app.use("/js", express.static("./public/js"));
+app.use("/html", express.static("./public/html"));
+app.use("/images", express.static("./public/images"));
+app.use("/json", express.static("./json"));
+app.use("/CustomComponents", express.static("./public/components"));
+app.use("/download", express.static("./public/download"));
 
-app.use('/', require('./routes/index'));
+app.use("/", require("./routes/index"));
 
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(favicon(__dirname + "/public/images/favicon.ico"));
 
 /* Starting app on port specified above */
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
 });
