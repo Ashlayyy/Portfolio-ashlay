7c22c5ca38830eb629300ed04fab264f4fdab482
Ashlay <ashlay.prive@proton.me>
2025-06-11 21:47:53 +0200

    Refactored Data class in Data.js for improved code consistency and readability by standardizing indentation and formatting in the constructor and getData method.

--- public/js/utils/Data.js
+++ public/js/utils/Data.js
@@ -1,16 +1,15 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 class Data {
-    data = undefined;
+	data = undefined;
 
-    constructor () {
-        this.api = new Api();
-        this.data = this.getData();
-    }
+	constructor() {
+		this.api = new Api();
+		this.data = this.getData();
+	}
 
-    getData = async () => {
-        let data = await (this.api.getData('/json/data.json'));
-        return data;
-    }
+	getData = async () => {
+		let data = await this.api.getData('json/data.json');
+		return data;
+	};
 }
-
